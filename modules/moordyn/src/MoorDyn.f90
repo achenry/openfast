b2176d5ac9ef7c1b8d8217b707a3c991351a2316
Matt Hall <5151457+mattEhall@users.noreply.github.com>
2021-09-01 12:15:22 -0600

    Specify usflowt in MD version message

--- modules/moordyn/src/MoorDyn.f90
+++ modules/moordyn/src/MoorDyn.f90
@@ -30,7 +30,7 @@ MODULE MoorDyn
 
    PRIVATE
 
-   TYPE(ProgDesc), PARAMETER            :: MD_ProgDesc = ProgDesc( 'MoorDyn-F', 'v2.a9', '24 March 2021' )
+   TYPE(ProgDesc), PARAMETER            :: MD_ProgDesc = ProgDesc( 'MoorDyn-F', 'v2.usflowt10', '1 Sept 2021' )
 
    INTEGER(IntKi), PARAMETER            :: wordy = 0   ! verbosity level. >1 = more console output
 
